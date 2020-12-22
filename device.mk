73499ac0f84ed662b2f8751b8365c3334ef0b03b
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-12-22 19:59:09 +0100

    X01AD: Upgrade BT stack from sunfish
    - Also disable A2DP offload
    
    * From: sunfish-user 11 RP1A.200720.011 6746289 release-keys
    
    Change-Id: Ia5782ca507690d459da7bccf949f87e6de6a2478
    Signed-off-by: danascape <saalimquadri1@gmail.com>

--- device.mk
+++ device.mk
@@ -159,12 +159,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
-
-PRODUCT_PACKAGES += \
-    libbthost_if
+    android.hardware.bluetooth.audio@2.0-impl
 
 # Camera
 PRODUCT_PACKAGES += \
