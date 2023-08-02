10eb46c4edaab493857efe55e06a31c9a079bdcd
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-02 16:01:44 +0700

    DotOs changes

--- dot_X01AD.mk
+++ dot_X01AD.mk
@@ -17,6 +17,13 @@
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Set shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
 # Inherit some common DotOS stuff.
 $(call inherit-product, vendor/dot/config/common.mk)
 
