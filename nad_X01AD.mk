1fc98a9283fc35f12529a5496d024cd8e809e5b4
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-02 14:08:29 +0700

    X01AD: Nusantara changes

--- nad_X01AD.mk
+++ nad_X01AD.mk
@@ -17,8 +17,16 @@
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Set shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
 # Inherit some common NusantaraProject stuff
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
 
 # Extra Flags
 NAD_BUILD_TYPE := UNOFFICIAL
