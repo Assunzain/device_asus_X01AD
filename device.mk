588b71c4e9a30a3e6a0aaa259c6892aaf2a9e5ca
Assunzain <asepsunanda.zain@gmail.com>
2023-08-06 01:43:49 +0000

    X01AD: Addapt libperf powerhint onclite to X01AD * https://github.com/angel-1999-stack/android_device_xiaomi_onclite_13/commit/25e1ef10a1f54e78b5e01c0739b04976d5d4bb75

--- device.mk
+++ device.mk
@@ -357,6 +357,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
+    android.hardware.power-service.X01AD-libperfmgr
+
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
