2661cb3ffd3e7e8b01fb44e97d2d3c14788c3b13
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-15 12:48:54 +0700

    X01AD: changes display hals

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 rm -rf hardware/qcom-caf/msm8996/audio
 rm -rf hardware/qcom-caf/msm8996/media
 rm -rf hardware/qcom-caf/msm8996/display
-git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_display -b thirteen hardware/qcom-caf/msm8996/display
+git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display -b 13 hardware/qcom-caf/msm8996/display
 git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_media -b thirteen hardware/qcom-caf/msm8996/media
 git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_audio -b thirteen hardware/qcom-caf/msm8996/audio
