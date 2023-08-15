e058e8223e4f31b6b9cf2002cb7a1efe5510dc9f
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-16 01:59:37 +0700

    X01AD: changes display hals & remove clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,6 @@
-rm -rf prebuilts/clang/host/linux-x86/clang-proton
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
 rm -rf hardware/qcom-caf/msm8996/audio
 rm -rf hardware/qcom-caf/msm8996/media
 rm -rf hardware/qcom-caf/msm8996/display
-git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_display -b thirteen hardware/qcom-caf/msm8996/display
+git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display -b 13 hardware/qcom-caf/msm8996/display
 git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_media -b thirteen hardware/qcom-caf/msm8996/media
 git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_audio -b thirteen hardware/qcom-caf/msm8996/audio
