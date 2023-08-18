da7ff708c7b594b80d8fccfe5ea2929b0fbb35ab
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-18 18:15:44 +0700

    X01AD: CameraGo  changes

--- GoogleCameraGo/Android.mk
+++ GoogleCameraGo/Android.mk
@@ -1,13 +1,10 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := GoogleCameraGo
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := GoogleCameraGo.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_OVERRIDES_PACKAGES := Snap Camera2 Aperture
-LOCAL_DEX_PREOPT := false
-LOCAL_PRODUCT_MODULE := true
-include $(BUILD_PREBUILT)
+android_app_import {
+	name: "GoogleCameraGo",
+	owner: "google",
+	apk: "GoogleCameraGo.apk",
+	presigned: true,
+	dex_preopt: {
+		enabled: false,
+	},
+	product_specific: true,
+	overrides: ["Camera2", "Snap"],
