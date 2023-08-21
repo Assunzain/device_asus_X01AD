a3812b4a886842ce0130a26090ef685368e2409b
Assunzain <Asepsunanda.zain@gmail.com>
2023-08-21 16:41:40 +0700

    X01AD: add GcamGo prebuild

--- device.mk
+++ device.mk
@@ -49,6 +49,10 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 PRODUCT_PACKAGES += \
 disable_configstore
 
+# Gcam Product
+PRODUCT_PACKAGES += \
+    GcamGoMod
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1520
 TARGET_SCREEN_WIDTH := 720
