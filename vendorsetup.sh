78a856455aea26c87bbdc5484a906768ece0a349
Assunzain <asepsunanda.zain@gmail.com>
2023-08-23 05:43:03 +0000

    X01AD: remove hals

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,10 @@
 # Remove hals
-rm -rf hardware/qcom-caf/msm8996/audio
-rm -rf hardware/qcom-caf/msm8996/media
 rm -rf hardware/qcom-caf/msm8996/display
 
 # Import Hals
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display -b 13 hardware/qcom-caf/msm8996/display
-git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_media -b thirteen hardware/qcom-caf/msm8996/media
-git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_audio -b thirteen hardware/qcom-caf/msm8996/audio
 
 # import Cameras
-rm -rf packages/apps/Camera2
-git clone https://github.com/ArrowOS/android_packages_apps_Camera2 -b arrow-13.1 packages/apps/Camera2
-rm -rf hardware/google/camera
-git clone https://github.com/RisingTechOSS/android_hardware_google_camera -b thirteen hardware/google/camera
 rm -rf packages/apps/Aperture
 git clone https://github.com/LineageOS/android_packages_apps_Aperture -b lineage-20.0 packages/apps/Aperture
 
