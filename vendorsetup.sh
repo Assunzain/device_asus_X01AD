cf921cbae3e5f5877f889fd2a5d3e35de53953c9
Assunzain <asepsunanda.zain@gmail.com>
2023-08-28 19:16:42 +0000

    X01AD: add viper4 and other flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,12 +4,11 @@ rm -rf hardware/qcom-caf/msm8996/display
 # Import Hals
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display -b 13 hardware/qcom-caf/msm8996/display
 
-# import Cameras
-rm -rf packages/apps/Aperture
-git clone https://github.com/LineageOS/android_packages_apps_Aperture -b lineage-20.0 packages/apps/Aperture
+# Import Viper
+git clone https://github.com/Assunzain/vendor_v4afx -b main vendor/v4afx
 
 # import Kernel GreenLight v2.2 KSU
-git clone https://github.com/Assunzain/Kernel_asus_X01AD -b GLKernelSU-non-oc kernel/asus/X01AD --depth=1
+git clone https://github.com/Assunzain/Kernel_asus_X01AD -b Perf{KSU} kernel/asus/X01AD --depth=1
 
 # import vendor
 git clone https://github.com/Assunzain/vendor_asus_X01AD_A13 -b 13 vendor/asus/X01AD
