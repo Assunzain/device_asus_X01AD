06d3c81685dc0e2931754e2d97c79b121be0aed7
Assunzain <asepsunanda.zain@gmail.com>
2023-08-29 08:42:24 +0000

    Revert "X01AD: add libbqiob fix error"
    This reverts commit 7258e97fd3115d5ff40beac71eaf292b78f9ee4b.

--- device.mk
+++ device.mk
@@ -54,10 +54,6 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 1520
 TARGET_SCREEN_WIDTH := 720
 
-# add libbqio
-PRODUCT_PACKAGES += \
-    libbfqio
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
