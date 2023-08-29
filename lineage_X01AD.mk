baee6316cecc1cbc2e5b96ecc1b69f58b12dfcae
Assunzain <asepsunanda.zain@gmail.com>
2023-08-29 12:08:50 +0000

    X01AD: BringUp AlphaDroid

--- lineage_X01AD.mk
+++ lineage_X01AD.mk
@@ -25,31 +25,40 @@ $(call inherit-product, device/asus/X01AD/device.mk)
 # Inherit some common Lineage OS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-
-
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
-
-
-# RisingOS Flags
-RISING_CHIPSET := Snapdragon 632
-RISING_MAINTAINER := AssunZain
-RISING_DEVICE := X01AD
-TARGET_ENABLE_BLUR := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # set viper4fx
 $(call inherit-product, vendor/v4afx/config.mk)
 
-TARGET_BOOT_ANIMATION_RES := 720
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_BOOT_ANIMATION_RES := 720
 TARGET_BUILD_APERTURE_CAMERA := true
 TARGET_USE_PIXEL_FINGERPRINT := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_HAS_UDFPS := false
-WITH_GMS := false
+
+# GAPPS config
+# 0 - NO GAPPS (default)
+# 1 - CORE GAPPS
+# 2 - FULL GAPPS
+WITH_GAPPS := 1
+
+# Use Google Telephony pack (Dialer, Contacts, Messaging) on GAPPS builds
+# Default = true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# Debugging
+# Default = false
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := UNOFFICIAL
+ALPHA_MAINTAINER := AssunZain
 
 # Device identifiers.
 PRODUCT_NAME := lineage_X01AD
