001d98d2cd53b34031999ba5fc8bfd145fdfded1
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-29 19:43:34 +0700

    X01AD: add another packages

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,8 +4,11 @@ rm -rf hardware/qcom-caf/msm8996/display
 # Import Hals
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display -b 13 hardware/qcom-caf/msm8996/display
 
-rm -rf hardware/qcom-caf/wlan
-git clone https://github.com/PixelExperience/hardware_qcom-caf_wlan -b thirteen hardware/qcom-caf/wlan
+# Import Packagest 
+git clone https://github.com/PixelExperience/packages_apps_FaceUnlockService -b thirteen packages/apps/FaceUnlockService
+
+# Import Viper
+git clone https://github.com/Assunzain/vendor_v4afx -b main vendor/v4afx
 
 # import Kernel GreenLight v2.2 KSU
 git clone https://github.com/Assunzain/Kernel_asus_X01AD -b GLKernelSU-non-oc kernel/asus/X01AD --depth=1
