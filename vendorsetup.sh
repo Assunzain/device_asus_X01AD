49b121e00a696604dd41dedd45e67aed200d91a9
Assunzain <asepsunanda.zain@gmail.com>
2023-09-06 01:08:03 +0700

    X01AD: changed hals to lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,14 @@
 rm -rf hardware/qcom-caf/msm8996/audio
 rm -rf hardware/qcom-caf/msm8996/media
 rm -rf hardware/qcom-caf/msm8996/display
-git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display -b 13 hardware/qcom-caf/msm8996/display
-git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_media -b thirteen hardware/qcom-caf/msm8996/media
-git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_audio -b thirteen hardware/qcom-caf/msm8996/audio
+
+# Import Hals
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
 
 # Import Viper
 git clone https://github.com/Assunzain/vendor_v4afx -b main vendor/v4afx
-
-# import Cameras
-rm -rf packages/apps/Aperture
-git clone https://github.com/LineageOS/android_packages_apps_Aperture -b lineage-20.0 packages/apps/Aperture
 
 # import Kernel GreenLight v2.2 KSU
 git clone https://github.com/Assunzain/Kernel_asus_X01AD -b Perf{KSU} kernel/asus/X01AD --depth=1
