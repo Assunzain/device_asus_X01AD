6b9b51718e1da912347ea5877a6a59386734ca91
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-09-12 19:45:40 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,18 @@
 # Remove hals
+rm -rf hardware/qcom-caf/msm8996/audio
+rm -rf hardware/qcom-caf/msm8996/media
 rm -rf hardware/qcom-caf/msm8996/display
 
 # Import Hals
-git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display -b 13 hardware/qcom-caf/msm8996/display
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
 
 rm -rf hardware/qcom-caf/wlan
 git clone https://github.com/PixelExperience/hardware_qcom-caf_wlan -b thirteen hardware/qcom-caf/wlan
 
 # import Kernel GreenLight v2.2 KSU
-git clone https://github.com/Assunzain/Kernel_asus_X01AD -b GLKernelSU-non-oc kernel/asus/X01AD --depth=1
+git clone https://github.com/Assunzain/Kernel_asus_X01AD -b Perf{KSU} kernel/asus/X01AD --depth=1
 
 # import vendor
 git clone https://github.com/Assunzain/vendor_asus_X01AD_A13 -b 13 vendor/asus/X01AD
