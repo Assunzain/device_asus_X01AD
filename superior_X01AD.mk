95c329b24c18e4f46696caaa8192cf8930abf70f
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-09-25 17:20:47 +0700

    X01AD: Re-chech common superior

--- superior_X01AD.mk
+++ superior_X01AD.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/X01AD/device.mk)
 
 # Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
