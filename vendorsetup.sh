6d3d9303f2f1ec0b22ed8bd5acec6f0e8d5aaddb
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-09-25 14:10:36 +0700

    X01AD: Changed Kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,3 @@
-# Remove hals
-rm -rf hardware/qcom-caf/msm8996/audio
-rm -rf hardware/qcom-caf/msm8996/media
-rm -rf hardware/qcom-caf/msm8996/display
-
-# Import Hals
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/display
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/media
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
 
 # import Cameras
 rm -rf packages/apps/Camera2
@@ -17,10 +8,13 @@ rm -rf packages/apps/Aperture
 git clone https://github.com/LineageOS/android_packages_apps_Aperture -b lineage-20.0 packages/apps/Aperture
 
 # import Kernel GreenLight v2.2 KSU
-git clone https://github.com/Assunzain/Kernel_asus_X01AD -b 1 kernel/asus/X01AD --depth=1
+git clone https://github.com/Assunzain/Kernel_asus_X01AD -b Perf{KSU} kernel/asus/X01AD --depth=1
+
+# Import Viper
+git clone https://github.com/Assunzain/vendor_v4afx -b main vendor/v4afx
 
 # import vendor
 git clone https://github.com/Assunzain/vendor_asus_X01AD_A13 -b 13 vendor/asus/X01AD
 
 # import clang proton support
-git clone https://github.com/kdrag0n/proton-clang -b master --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b -b master --depth=1 prebuilts/clang/host/linux-x86/clang-r437112b
